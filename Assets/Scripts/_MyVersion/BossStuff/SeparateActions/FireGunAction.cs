650c35c347ac9ed0c0afe0686141da611e5f4d7c
Scott Batey <sebateyjr@yahoo.com>
2022-11-03 16:02:23 -0400

    Removed additional Health Script, Added namespace to FireGunAction

--- Assets/Scripts/_MyVersion/BossStuff/SeparateActions/FireGunAction.cs
+++ Assets/Scripts/_MyVersion/BossStuff/SeparateActions/FireGunAction.cs
@@ -2,43 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireGunAction : BossAction
+namespace TheFrozenBanana
 {
-	[SerializeField] private GameObject ProjectilePrefab;
-	[SerializeField] private int ShotsPerAction;
-	[SerializeField] private float AngleChangePerShot;
 
-	protected override IEnumerator CarryOutSpecificAction() {
-		float t = 0f;
-		int shot = 1;
-		float horizontalSign = Mathf.Sign(BossManager.Target.position.x - transform.position.x);
-		float verticalSign = Mathf.Sign(BossManager.Target.position.y-(2f) - transform.position.y);
-		Vector2 direction = new Vector2(horizontalSign, verticalSign);
-		float startAngle = -30f;
-		if (direction.y > 0) {
-			startAngle = 15f;
+	public class FireGunAction : BossAction
+	{
+		[SerializeField] private GameObject ProjectilePrefab;
+		[SerializeField] private int ShotsPerAction;
+		[SerializeField] private float AngleChangePerShot;
+
+		protected override IEnumerator CarryOutSpecificAction()
+		{
+			float t = 0f;
+			int shot = 1;
+			float horizontalSign = Mathf.Sign(BossManager.Target.position.x - transform.position.x);
+			float verticalSign = Mathf.Sign(BossManager.Target.position.y - (2f) - transform.position.y);
+			Vector2 direction = new Vector2(horizontalSign, verticalSign);
+			float startAngle = -30f;
+			if (direction.y > 0)
+			{
+				startAngle = 15f;
+			}
+			Debug.Log(startAngle);
+			float angle = startAngle;
+			float deltaTime = TotalActionTime / ShotsPerAction;
+			while (t < TotalActionTime)
+			{
+				yield return new WaitForSeconds(deltaTime);
+				t += deltaTime;
+				CreateProjectile(direction, angle);
+				shot++;
+				angle += AngleChangePerShot;
+			}
+			_actionBusy = false;
 		}
-		Debug.Log(startAngle);
-		float angle = startAngle;
-		float deltaTime = TotalActionTime / ShotsPerAction;
-		while (t < TotalActionTime) {
-			yield return new WaitForSeconds(deltaTime);
-			t += deltaTime;
-			CreateProjectile(direction, angle);
-			shot++;
-			angle += AngleChangePerShot;
+
+		private void CreateProjectile(Vector2 direction, float angle)
+		{
+			Quaternion rotation = Quaternion.Euler(0, 0, angle);
+			if (direction.x < 0)
+			{
+				rotation = Quaternion.Euler(0, 0, -angle);
+				rotation *= Quaternion.Euler(0, 0, 180);
+			}
+			GameObject tmp = Instantiate(ProjectilePrefab, BossManager.Weapon.transform.position, Quaternion.identity, null) as GameObject;
+			Vector3 fireDirection = tmp.transform.position + rotation * Vector3.right * 20;
+			tmp.GetComponent<Projectile>().Setup(tmp.transform.position, fireDirection, rotation, "Enemy");
 		}
-		_actionBusy = false;
 	}
 
-	private void CreateProjectile(Vector2 direction, float angle) {
-		Quaternion rotation = Quaternion.Euler(0,0,angle);
-		if (direction.x < 0) {
-			rotation = Quaternion.Euler(0, 0, - angle);
-			rotation *= Quaternion.Euler(0,0,180);
-		}
-		GameObject tmp = Instantiate(ProjectilePrefab, BossManager.Weapon.transform.position, Quaternion.identity, null) as GameObject;
-		Vector3 fireDirection = tmp.transform.position + rotation * Vector3.right * 20;
-		tmp.GetComponent<Projectile>().Setup(tmp.transform.position, fireDirection, rotation ,"Enemy");
-	}
 }
