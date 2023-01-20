b7451f2d9e5a17145d39ab5a4cf3208bdcedb209
Pixelwortel <97120922+Pixelwortel@users.noreply.github.com>
2023-01-20 13:37:52 +0100

    Drone additions - sprite and multi-fire

--- Assets/Scripts/EnemyAI/AI/SeparateActions/EnemyFireAction.cs
+++ Assets/Scripts/EnemyAI/AI/SeparateActions/EnemyFireAction.cs
@@ -6,9 +6,12 @@ namespace TheFrozenBanana
 {
     public class EnemyFireAction : EnemyAction
     {
-		[SerializeField] private GameObject _projectileWeapon;
+		[SerializeField] private GameObject[] _projectileWeapon;
 		[SerializeField] private float _aimSpeed;
-		[SerializeField] private GameObject _chargeParticles;
+		[SerializeField] private GameObject _chargeParticleEffect;
+		[SerializeField] private int shotsPerVolley = 1;
+		[SerializeField] private float volleyTime = 1;
+		private int currentWeaponId = 0;
 
 		protected override IEnumerator CarryOutAction() {
 			if (_showDebugLog) {
@@ -17,9 +20,11 @@ namespace TheFrozenBanana
 			_actionInEffect = true;
 			float t = 0;
 			float angle = 0;
-			if (_chargeParticles != null) {
-				GameObject partSys = Instantiate(_chargeParticles, _projectileWeapon.transform.position, Quaternion.identity, _projectileWeapon.transform) as GameObject;
-				Destroy(partSys,_actionTime);
+			if (_chargeParticleEffect != null) {
+				foreach (GameObject weap in _projectileWeapon) {
+					GameObject partSys = Instantiate(_chargeParticleEffect, weap.transform.position, Quaternion.identity, weap.transform) as GameObject;
+					Destroy(partSys, _actionTime * (shotsPerVolley/_projectileWeapon.Length));
+				}
 			}
 			while (t < _actionTime) {
 				if (!_actionInEffect) {
@@ -32,23 +37,38 @@ namespace TheFrozenBanana
 				_locomotion.HorizontalLook = Mathf.Sign(ec.Target.transform.position.x - gameObject.transform.position.x);
 
 				// Angle handling
-				Vector3 vectorToTarget = ec.Target.transform.position - _projectileWeapon.transform.position;
-				angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-				if (ec.gameObject.transform.localScale.x < 0) {
-					angle += 180;
+				foreach (GameObject weap in _projectileWeapon) {
+					Vector3 vectorToTarget = ec.Target.transform.position - weap.transform.position;
+					angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+					if (ec.gameObject.transform.localScale.x < 0) {
+						angle += 180;
+					}
+					Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+					weap.transform.rotation = Quaternion.Slerp(weap.transform.rotation, q, Time.deltaTime * _aimSpeed);
 				}
-				Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-				_projectileWeapon.transform.rotation = Quaternion.Slerp(_projectileWeapon.transform.rotation, q, Time.deltaTime * _aimSpeed);
-
 				t += Time.deltaTime;
 				yield return new WaitForEndOfFrame();
 			}
-			
-			if (_actionInEffect) {
-				ec.EnemyAttack(true);
-				yield return new WaitForEndOfFrame();
-				ec.EnemyAttack(false);
-				_inputManager.EndOverride();
+			/*
+			 *			if (_actionInEffect) {
+
+							ec.EnemyAttack(true);
+							yield return new WaitForEndOfFrame();
+							ec.EnemyAttack(false);
+							_inputManager.EndOverride();
+
+							}*/
+			int shotsFired = 0;
+			while (shotsFired < shotsPerVolley) {
+				float shotTime = volleyTime / (float)shotsPerVolley;
+				foreach (GameObject weap in _projectileWeapon) {
+					weap.GetComponent<IWeapon>().Attack(shotTime);
+					yield return new WaitForSeconds(shotTime);
+					shotsFired++;
+					if (shotsFired >= shotsPerVolley) {
+						break;
+					}
+				}
 			}
 			StopAction();
 		}
@@ -62,11 +82,15 @@ namespace TheFrozenBanana
 				if (this._actionInEffect) {
 					yield break;
 				}
-				_projectileWeapon.transform.rotation = Quaternion.Slerp(_projectileWeapon.transform.rotation, q, t);
+				foreach (GameObject weap in _projectileWeapon) {
+					weap.transform.rotation = Quaternion.Slerp(weap.transform.rotation, q, t);
+				}
 				t += Time.deltaTime;
 				yield return new WaitForEndOfFrame();
 			}
-			_projectileWeapon.transform.rotation = Quaternion.Slerp(_projectileWeapon.transform.rotation, q, 1);
+			foreach (GameObject weap in _projectileWeapon) {
+				weap.transform.rotation = Quaternion.Slerp(weap.transform.rotation, q, 1);
+			}
 		}
 	}
 }
